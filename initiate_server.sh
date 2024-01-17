e9cfcda410231d9b16a74bf5bc4781f7f5abba2c
José María FR <josemaria.fernandez.rodriguez@alumnos.upm.es>
2024-01-17 20:43:22 +0100

    Docker install added to script

--- initiate_server.sh
+++ initiate_server.sh
@@ -1,5 +1,22 @@
-sudo apt update
-sudo apt install -y pip git wget zip unzip
+## DOCKER ENGINE INSTALL
+
+# Add Docker's official GPG key:
+sudo apt-get update
+# Install required packages (and additionals needed for later)
+sudo apt-get install -y pip git wget zip unzip ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+# Install all docker packages
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 ## SDK INSTALL ##
 
